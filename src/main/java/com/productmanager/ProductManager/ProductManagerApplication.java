4ec312a1b3e69e70da1acb62745b9973b49a3e12
Tom Spencer <tom@homeflow.co.uk>
2020-01-12 12:46:30 +0000

    Add full CRUD actions for Spring Boot

---
+++ src/main/java/com/productmanager/ProductManager/ProductManagerApplication.java
@@ -0,0 +1,14 @@
+package com.productmanager.ProductManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProductManagerApplication.class, args);
+	}
+
+}
