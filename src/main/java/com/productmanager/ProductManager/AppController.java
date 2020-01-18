076c7ae05c62a4d9d21b0448f4b545aed2281a05
Tom <tom@homeflow.co.uk>
2020-01-18 16:54:08 +0000

    Edit cors for controller

--- src/main/java/com/productmanager/ProductManager/AppController.java
+++ src/main/java/com/productmanager/ProductManager/AppController.java
@@ -3,16 +3,13 @@ package com.productmanager.ProductManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @Controller
-@CrossOrigins("*", methods = { RequestMethod.GET })
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET })
 public class AppController {
     @Autowired
     private ProductService service;
